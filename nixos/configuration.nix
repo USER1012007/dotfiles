35587eb5f708d2f9b10a4ac42160b9f6fc1b17f7
User1012007 <rojasbadilloe@gmail.com>
2025-07-23 19:59:11 -0600

    nixos: Change systemd-boot to grub

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -24,18 +24,26 @@ in
   # Bootloader.
   boot.kernelModules = [ "nvidia_uvm" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.grub = {
-    # enable = true;
-    useOSProber = true;
-    device = "nodev";
-  };
 
-  boot.loader.gummiboot = {
-    enable = true;
-    timeout = 3;
-  };
+  # boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.grub = {
+  #   # enable = true;
+  #   useOSProber = true;
+  #   device = "nodev";
+  # };
+
+  # boot.loader.gummiboot = {
+  #   enable = true;
+  #   timeout = 3;
+  # };
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
 
   hardware.graphics = {
     enable = true;
