8de64a5eb53d2e54bde0e8033d98aac25c6c57a3
User1012007 <rojasbadilloe@gmail.com>
2025-08-14 19:52:10 -0600

    Clean comments

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 # command to active unstable packages
 # nix-channel --add https://channels.nixos.org/nixos-unstable nixos
 
@@ -16,27 +13,15 @@ let
 in
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
-  hardware.bluetooth.enable = true; # enables support for bluetooth
-  hardware.bluetooth.powerOnBoot = false; # powers up the default Bluetooth controller on boot
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = false; 
+
   # Bootloader.
   boot.kernelModules = [ "nvidia_uvm" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
-
-  # boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.grub = {
-  #   # enable = true;
-  #   useOSProber = true;
-  #   device = "nodev";
-  # };
-
-  # boot.loader.gummiboot = {
-  #   enable = true;
-  #   timeout = 3;
-  # };
   boot.loader.systemd-boot.enable = false;
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
@@ -49,70 +34,46 @@ in
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = true;
 
-    hardware.graphics = {
-      enable = true;
-    };
-
+  hardware.graphics = {
+    enable = true;
+  };
   hardware.opengl.enable = true;
-  #hardware.graphics.enable32Bit = true;
-  #hardware.pulseaudio.support32Bit = true;
-
   hardware.nvidia = {
     modesetting.enable = true;
-   #powerManagment.enable = false;
-   #powerManagment.finegrained = true;
     open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
-  # Load nvidia driver fpr Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
 
   networking.hostName = "user1012007"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Mexico_City";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
   # virtualisation.docker.enable = true;
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.emilio = {
     isNormalUser = true;
     description = "emilio";
-    extraGroups = [ "networkmanager" "wheel" "audio"];
+    extraGroups = [ "networkmanager" "wheel" "audio" ];
     packages = with pkgs; [];
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.permittedInsecurePackages = [
      "libxml2-2.13.8"
   ];
-
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
 # Steam configurations
   programs.steam.enable = true;
-
   programs.xwayland.enable = true;
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
 
   environment.systemPackages = with pkgs; [
      (pkgs.python3.withPackages (python-pkgs: [
@@ -184,6 +145,7 @@ in
      sqlite-web
      nodejs_24
      php
+     php84Packages.composer
      # kotlin
      # flutter
      # rustc
@@ -215,10 +177,6 @@ in
      # wf-recorder # to record screen # wf-recorder --audio=alsa_output.usb-Razer_Razer_Kraken_V3_X_00000000-00.pro-output-0.monitor --c=H.264 --file=recording.mp4
   ];
 
-  # services.vnstat = {
-  #   enable = true; # Habilita el servicio vnstat
-  # };
-
   fonts.packages = with pkgs; [
     fira-code
     fira-code-symbols
@@ -247,18 +205,12 @@ in
     openFirewall = true;
   };
   
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
-  # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
   xdg.portal.config.common.default = "*";
   xdg.portal = {
     enable = true;
@@ -275,7 +227,6 @@ in
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     #jack.enable = true;
   };
 
@@ -312,29 +263,11 @@ in
     interfaces."wlp4s0".allowedUDPPorts = [ 5353 45259 34445 ];
     interfaces."wlp4s0".allowedTCPPorts = [  ];
 
-    # Optional: disable responding to ping from WAN
     allowPing = false;
-
-    # Optional: log blocked packets for review
     logRefusedConnections = true;
   };
 
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.05"; 
   
  #PS1='\[\e[0m\][\[\e[1;36m\]\u\[\e[0m\]@\[\e[1;36m\]\h\[\e[0m\] \W]\$ '
   # Definir PROMPT_COMMAND en NixOS
