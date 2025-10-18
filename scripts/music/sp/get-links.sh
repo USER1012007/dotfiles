e8155bb1f906d05d70a1d97837d357e4bd2379bf
User1012007 <rojasbadilloe@gmail.com>
2025-10-18 10:00:42 -0600

    sp: add new dir for scripts to download with sp

---
+++ scripts/music/sp/get-links.sh
@@ -0,0 +1,32 @@
+# get the metadata from the .json
+# jq -r '.items[] | "\(.track.name)|\(.track.artists[0].name)|\(.track.album.name)|\(.track.album.images[0].url)"' premuroso.json
+
+if [ -z "$1" ]; then
+    echo "Uso: $0 <directorio>"
+    exit 1
+fi
+
+DIR="$1"
+URL=""
+
+if [ ! -d "$DIR" ]; then
+    echo "❌ '$DIR' no es un directorio válido."
+    exit 1
+fi
+
+for file in "$DIR"/*.txt; do
+    [[ -f "$file" ]] || continue  
+    
+    grep -oP '^[^|]+\|[^|]+' "$file" | sed 's/|/,/' | while IFS= read -r query; do
+        [[ -z "$query" ]] && continue  
+        
+        URL=$(yt-dlp "ytsearch1:$query" --get-id 2>/dev/null)
+
+        if [[ -n "$URL" ]]; then
+            echo "https://www.youtube.com/watch?v=$URL"
+        else
+            echo "❌ No se encontró resultado para: $query"
+            echo $query > missing.txt
+        fi
+    done
+done
