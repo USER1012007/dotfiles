7348ff31abd18df2bebbb18159c3bc58eda8f774
User1012007 <rojasbadilloe@gmail.com>
2026-02-13 12:08:25 -0600

    home.nix: add nvim extraPackage configuration to maintain nvim packages just for nvim

--- home.nix
+++ home.nix
@@ -65,6 +65,36 @@ in
     };
   };
 
+  programs.neovim = {
+    enable = true;
+
+    extraPackages = with pkgs; [
+       # nvchad plugins
+       ripgrep
+       nil     
+       lua-language-server
+       xclip   
+
+       clang-tools
+       rust-analyzer
+       lua-language-server
+       lombok
+       jdt-language-server
+       nodejs
+
+       # vim plugins
+       vimPlugins.lazygit-nvim
+       vimPlugins.plenary-nvim
+       vimPlugins.vim-visual-multi
+       vimPlugins.neoformat
+       vimPlugins.nvim-jdtls
+    ];
+
+    extraPython3Packages = python3Packages: with python3Packages; [
+      pynvim
+    ];
+  };
+
   # programs.zed-editor = {
   #   extensions = [
   #     "log"
@@ -242,24 +272,24 @@ in
   xdg.configFile."niri/config.kdl".source = ./configs/niri/config.kdl;
   xdg.configFile."waybar/config".source = ./configs/waybar/config;
   xdg.configFile."waybar/style.css".source = ./configs/waybar/style.css;
-  xdg.configFile."eww/eww.scss".source = ./configs/eww/eww.scss;
-  xdg.configFile."eww/eww.yuck".source = ./configs/eww/eww.yuck;
-  xdg.configFile."eww/modules/battery.yuck".source = ./configs/eww/modules/battery.yuck;
-  xdg.configFile."eww/modules/clock.yuck".source = ./configs/eww/modules/clock.yuck;
-  xdg.configFile."eww/modules/cpu.yuck".source = ./configs/eww/modules/cpu.yuck;
-  xdg.configFile."eww/modules/focused-window.yuck".source = ./configs/eww/modules/focused-window.yuck;
-  xdg.configFile."eww/modules/network.yuck".source = ./configs/eww/modules/network.yuck;
-  xdg.configFile."eww/modules/niri-workspaces.yuck".source =
-    ./configs/eww/modules/niri-workspaces.yuck;
-  xdg.configFile."eww/modules/ram.yuck".source = ./configs/eww/modules/ram.yuck;
-  xdg.configFile."eww/modules/temp.yuck".source = ./configs/eww/modules/temp.yuck;
-  xdg.configFile."eww/modules/calendar.yuck".source = ./configs/eww/modules/calendar.yuck;
-  xdg.configFile."eww/scripts/niri-focused-window.sh".source =
-    ./configs/eww/scripts/niri-focused-window.sh;
-  xdg.configFile."eww/scripts/niri-workspaces.sh".source = ./configs/eww/scripts/niri-workspaces.sh;
-  xdg.configFile."eww/scripts/nmcli-monitor.sh".source = ./configs/eww/scripts/nmcli-monitor.sh;
-  xdg.configFile."eww/scripts/calendar.sh".source = ./configs/eww/scripts/calendar.sh;
-  xdg.configFile."eww/scripts/multimonitor.sh".source = ./configs/eww/scripts/multimonitor.sh;
+  # xdg.configFile."eww/eww.scss".source = ./configs/eww/eww.scss;
+  # xdg.configFile."eww/eww.yuck".source = ./configs/eww/eww.yuck;
+  # xdg.configFile."eww/modules/battery.yuck".source = ./configs/eww/modules/battery.yuck;
+  # xdg.configFile."eww/modules/clock.yuck".source = ./configs/eww/modules/clock.yuck;
+  # xdg.configFile."eww/modules/cpu.yuck".source = ./configs/eww/modules/cpu.yuck;
+  # xdg.configFile."eww/modules/focused-window.yuck".source = ./configs/eww/modules/focused-window.yuck;
+  # xdg.configFile."eww/modules/network.yuck".source = ./configs/eww/modules/network.yuck;
+  # xdg.configFile."eww/modules/niri-workspaces.yuck".source =
+  #   ./configs/eww/modules/niri-workspaces.yuck;
+  # xdg.configFile."eww/modules/ram.yuck".source = ./configs/eww/modules/ram.yuck;
+  # xdg.configFile."eww/modules/temp.yuck".source = ./configs/eww/modules/temp.yuck;
+  # xdg.configFile."eww/modules/calendar.yuck".source = ./configs/eww/modules/calendar.yuck;
+  # xdg.configFile."eww/scripts/niri-focused-window.sh".source =
+  #   ./configs/eww/scripts/niri-focused-window.sh;
+  # xdg.configFile."eww/scripts/niri-workspaces.sh".source = ./configs/eww/scripts/niri-workspaces.sh;
+  # xdg.configFile."eww/scripts/nmcli-monitor.sh".source = ./configs/eww/scripts/nmcli-monitor.sh;
+  # xdg.configFile."eww/scripts/calendar.sh".source = ./configs/eww/scripts/calendar.sh;
+  # xdg.configFile."eww/scripts/multimonitor.sh".source = ./configs/eww/scripts/multimonitor.sh;
   # xdg.configFile."zed/tasks.json".source = ./configs/zed/tasks.json;
   # xdg.configFile."zed/themes/custom-theme-1.json".source = ./configs/zed/themes/custom-theme-1.json;
   # xdg.configFile."zed/settings.json".source = ./configs/zed/settings.json;
