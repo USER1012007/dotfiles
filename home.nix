8ec0ebe44286122fad3437490ef6feac921e7f1c
User1012007 <rojasbadilloe@gmail.com>
2026-04-25 17:30:00 -0600

    home.nix: separate into modules and import programs configurations

--- home.nix
+++ home.nix
@@ -1,17 +1,11 @@
 { pkgs, config, ... }:
-let
-  yazi-flavors = pkgs.fetchFromGitHub {
-    owner = "yazi-rs";
-    repo = "flavors";
-    rev = "68326b4ca4b5b66da3d4a4cce3050e5e950aade5";
-    hash = "sha256-nhIhCMBqr4VSzesplQRF6Ik55b3Ljae0dN+TYbzQb5s=";
-  };
-  yazi-lazygit = builtins.fetchGit {
-    url = "https://github.com/Lil-Dank/lazygit.yazi.git";
-    rev = "0e56060192d1ccd307664bf93b3d0beb1efe528e";
-  };
-in
 {
+  imports = [
+    ./gtk-qt.nix
+    ./yazi.nix
+    ./nvim.nix
+    ./foot.nix
+  ];
   home.stateVersion = "25.05";
   home.username = "emilio";
   home.homeDirectory = "/home/emilio";
@@ -21,237 +15,13 @@ in
   programs.bash.enable = true;
   programs.foot.enable = true;
   programs.mpv.enable = true;
-  # programs.zed-editor.enable = true;
   programs.yazi.enable = true;
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
-  gtk.enable = true;
   services.swayosd.enable = true;
+  gtk.enable = true;
 
   # Configs
-
-  # programs.eww = {
-  #   configDir = ./configs/eww/eww.yuck;
-  # };
-
-  programs.foot.settings = {
-    main = {
-      font = "CaskaydiaMonoNerdFont:size=14";
-      dpi-aware = false;
-    };
-    colors-dark = {
-      alpha = 0.7;
-      background = "000000";
-      foreground = "FFFFFF";
-      ## Normal/regular colors (color palette 0-7)
-      regular0 = "000000"; # black
-      regular1 = "cd0000"; # red
-      regular2 = "00cd00"; # green
-      regular3 = "cdcd00"; # yellow
-      regular4 = "87ceeb"; # blue
-      regular5 = "cd00cd"; # magenta
-      regular6 = "00cdcd"; # cyan
-      regular7 = "e5e5e5"; # white
-
-      ## Bright colors (color palette 8-15)
-      bright0 = "7f7f7f"; # bright black
-      bright1 = "ff0000"; # bright red
-      bright2 = "00ff00"; # bright green
-      bright3 = "ffff00"; # bright yellow
-      bright4 = "add8e6"; # bright blue
-      bright5 = "ff00ff"; # bright magenta
-      bright6 = "00ffff"; # bright cyan
-      bright7 = "ffffff"; # bright white
-    };
-  };
-
-  programs.neovim = {
-    enable = true;
-
-    extraPackages = with pkgs; [
-       # nvchad plugins
-       ripgrep
-       nil     
-       lua-language-server
-       xclip   
-
-       clang-tools
-       rust-analyzer
-       lua-language-server
-       lombok
-       jdt-language-server
-       nodejs
-       tree-sitter
-
-       # vim plugins
-       vimPlugins.lazygit-nvim
-       vimPlugins.plenary-nvim
-       vimPlugins.vim-visual-multi
-       vimPlugins.neoformat
-       vimPlugins.nvim-jdtls
-       gopls
-       gotools 
-       delve
-       go
-    ];
-
-    extraPython3Packages = python3Packages: with python3Packages; [
-      pynvim
-    ];
-  };
-
-  # programs.zed-editor = {
-  #   extensions = [
-  #     "log"
-  #     "nix"
-  #     "basher"
-  #     "typst"
-  #   ];
-  #   extraPackages = with pkgs; [
-  #     nil
-  #     nixfmt-rfc-style
-  #     tinymist
-  #     typstyle
-  #     clang-tools
-  #     python313Packages.python-lsp-server
-  #     python313Packages.pylint
-  #   ];
-  # };
-
-  programs.yazi = {
-    plugins = {
-      mount = pkgs.yaziPlugins.mount;
-      ouch = pkgs.yaziPlugins.ouch;
-    };
-    flavors = {
-      catppuccin-mocha = "${yazi-flavors}/catppuccin-mocha.yazi";
-    };
-    keymap = {
-      mgr.prepend_keymap = [
-        {
-          on = "M";
-          run = "plugin mount";
-          desc = "Open mount plugin";
-        }
-        {
-          on = "<S-j>";
-          run = "arrow 5";
-          desc = "Move cursor down 5 positions";
-        }
-        {
-          on = "<S-k>";
-          run = "arrow -5";
-          desc = "Move cursor up 5 positions";
-        }
-        {
-          on = [ "<C-n>" ];
-          run = "shell 'ripdrag \"$@\" -dx 2>/dev/null &' --confirm";
-        }
-        {
-          on = "C";
-          run = "plugin ouch";
-          desc = "Compress with ouch";
-        }
-        {
-          on   = [ "g" "i" ];
-          run  = "plugin lazygit";
-          desc = "run lazygit";
-        }
-        {
-          on   = [ "e" ];
-          run  = "shell --orphan --confirm foot";
-          desc = "run foot terminal";
-        }
-        {
-          on   = [ "u" ];
-          run  = "shell --orphan --confirm 'foot yazi'";
-          desc = "run yazi";
-        }
-        {
-          on = [ "<C-w>" ];
-          run = "close";
-          desc = "Close the current tab";
-        }
-      ];
-    };
-    settings = {
-      opener = {
-        play = [
-          {
-            run = "mpv \"$@\"";
-            orphan = true;
-            for = "unix";
-          }
-        ];
-        edit = [
-          {
-            run = "nvim \"$@\"";
-            block = true;
-            for = "unix";
-          }
-        ];
-        open = [
-          {
-            run = "xdg-open \"$@\"";
-            desc = "Open";
-          }
-        ];
-        extract = [
-          {
-            run = "unzip \"$@\"";
-            desc = "Extract here with unzip";
-            for = "unix";
-          }
-        ];
-      };
-      open = {
-        prepend_rules = [
-          {
-            name = "*.zip";
-            use = "extract";
-          }
-        ];
-      };
-      plugin = {
-        prepend_previewers = [
-          {
-            mime = "application/*zip";
-            run = "ouch";
-          }
-          {
-            mime = "application/x-tar";
-            run = "ouch";
-          }
-          {
-            mime = "application/x-bzip2";
-            run = "ouch";
-          }
-          {
-            mime = "application/x-7z-compressed";
-            run = "ouch";
-          }
-          {
-            mime = "application/x-rar";
-            run = "ouch";
-          }
-          {
-            mime = "application/x-xz";
-            run = "ouch";
-          }
-          {
-            mime = "application/xz";
-            run = "ouch";
-          }
-        ];
-      };
-    };
-    theme = {
-      flavor = {
-        # dark = "catppuccin-mocha";
-      };
-    };
-  };
-
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
@@ -259,24 +29,6 @@ in
     };
   };
 
-  gtk.gtk3.extraConfig = {
-    gtk-icon-theme-name = "Adwaita";
-    gtk-theme-name = "Adwaita-dark";
-    gtk-application-prefer-dark-theme = 1;
-    gtk-cursor-theme-name = "macOS";
-  };
-  gtk.gtk4.extraConfig = {
-    gtk-icon-theme-name = "Adwaita";
-    gtk-theme-name = "Adwaita-dark";
-    gtk-application-prefer-dark-theme = 1;
-    gtk-cursor-theme-name = "macOS";
-  };
-
-  qt = {
-    enable = true;
-    platformTheme.name = "qtct";
-    style.name = "kvantum";
-  };
 
   programs.mpv.config = {
     hwdec = "vaapi";
@@ -284,35 +36,12 @@ in
     gpu-api = "opengl";
   };
 
-  xdg.configFile."yazi/plugins/lazygit.yazi".source = yazi-lazygit;
   xdg.configFile."Thunar/uca.xml".source = ./configs/thunar.uca.xml;
   xdg.configFile."niri/config.kdl".source = ./configs/niri/config.kdl;
   xdg.configFile."waybar/config".source = ./configs/waybar/config;
   xdg.configFile."waybar/style.css".source = ./configs/waybar/style.css;
-  # xdg.configFile."eww/eww.scss".source = ./configs/eww/eww.scss;
-  # xdg.configFile."eww/eww.yuck".source = ./configs/eww/eww.yuck;
-  # xdg.configFile."eww/modules/battery.yuck".source = ./configs/eww/modules/battery.yuck;
-  # xdg.configFile."eww/modules/clock.yuck".source = ./configs/eww/modules/clock.yuck;
-  # xdg.configFile."eww/modules/cpu.yuck".source = ./configs/eww/modules/cpu.yuck;
-  # xdg.configFile."eww/modules/focused-window.yuck".source = ./configs/eww/modules/focused-window.yuck;
-  # xdg.configFile."eww/modules/network.yuck".source = ./configs/eww/modules/network.yuck;
-  # xdg.configFile."eww/modules/niri-workspaces.yuck".source =
-  #   ./configs/eww/modules/niri-workspaces.yuck;
-  # xdg.configFile."eww/modules/ram.yuck".source = ./configs/eww/modules/ram.yuck;
-  # xdg.configFile."eww/modules/temp.yuck".source = ./configs/eww/modules/temp.yuck;
-  # xdg.configFile."eww/modules/calendar.yuck".source = ./configs/eww/modules/calendar.yuck;
-  # xdg.configFile."eww/scripts/niri-focused-window.sh".source =
-  #   ./configs/eww/scripts/niri-focused-window.sh;
-  # xdg.configFile."eww/scripts/niri-workspaces.sh".source = ./configs/eww/scripts/niri-workspaces.sh;
-  # xdg.configFile."eww/scripts/nmcli-monitor.sh".source = ./configs/eww/scripts/nmcli-monitor.sh;
-  # xdg.configFile."eww/scripts/calendar.sh".source = ./configs/eww/scripts/calendar.sh;
-  # xdg.configFile."eww/scripts/multimonitor.sh".source = ./configs/eww/scripts/multimonitor.sh;
-  # xdg.configFile."zed/tasks.json".source = ./configs/zed/tasks.json;
-  # xdg.configFile."zed/themes/custom-theme-1.json".source = ./configs/zed/themes/custom-theme-1.json;
-  # xdg.configFile."zed/settings.json".source = ./configs/zed/settings.json;
 
   programs.bash = {
-    # Aliases
     shellAliases = {
       ls = "ls --color=auto";
       grep = "grep --color=auto";
