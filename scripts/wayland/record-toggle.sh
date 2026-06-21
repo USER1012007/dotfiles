e7ad78c807a2f12a8d252ea0430af4278b349f6d
User1012007 <rojasbadilloe@gmail.com>
2026-06-21 12:05:46 -0600

    record-toggle.sh: script to toggle record screen

---
+++ scripts/wayland/record-toggle.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+PIDFILE="/tmp/wl-screenrec.pid"
+SAVEDIR="$HOME/Pictures/screen_records"
+
+mkdir -p "$SAVEDIR"
+
+if [ -f "$PIDFILE" ]; then
+    kill -SIGINT "$(cat "$PIDFILE")"
+    rm -f "$PIDFILE"
+    notify-send "Grabación detenida" "Guardada en $SAVEDIR"
+else
+    FILENAME="$SAVEDIR/$(date '+%Y-%m-%d_%H-%M-%S').mp4"
+    wl-screenrec -f "$FILENAME" &
+    echo $! > "$PIDFILE"
+    notify-send "Grabando pantalla" "$FILENAME"
+fi
