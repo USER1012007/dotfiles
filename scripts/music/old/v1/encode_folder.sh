84b62927c50269e1bc1579b1ab7e60323232e112
User1012007 <rojasbadilloe@gmail.com>
2026-07-10 19:57:02 -0600

    v1: regroup an old and nostalgic version of my music scripts

---
+++ scripts/music/old/v1/encode_folder.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+input_folder="$1"
+
+ if [ ! -d "$input_folder" ]; then
+     echo "Error: Folder '$input_folder' not found."
+     exit 1
+ fi
+
+ for file in "$input_folder"/*.{mp3,wma,wav,flac}; do
+    if [ -f "$file" ]; then
+         filename=$(basename -- "$file")
+         extension="${filename##*.}"
+         filename_noext="${filename%.*}"
+  	
+	 output_file1="${file%.*}.opus"
+
+	 ffmpeg -i "$file" -map a:0 -c:a libopus -b:a 128k "$output_file1" #-map a:1 -c:a libopus -b:a 160k "$output_file2"
+
+     fi
+ done
+
+
+         read -p "Do you want to remove old files? [Y/n]: " ans
+
+         if [[ "$ans" == "y" || "$ans" == "Y" ]]; then
+
+		for file in "$input_folder"/*.{mp3,wma,wav,flac}; do
+		    if [[ -f "$file" ]]; then
+		    	rm "$file"
+			filename=$(basename -- "$file")
+		    	echo -e "\e[32m=>\e[0m \e[33m"$filename"\e[0m has been deleted"
+		    fi
+		done
+         fi
+	  
+
