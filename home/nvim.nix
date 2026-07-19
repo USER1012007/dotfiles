4b2665783d7e4074d15e1bc3afe73deda237f68b
User1012007 <rojasbadilloe@gmail.com>
2026-07-19 14:14:41 -0600

    nvim.nix: format and add typescript and vue lsp

--- home/nvim.nix
+++ home/nvim.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
 
   programs.neovim = {
 
@@ -8,42 +8,51 @@
     withPython3 = false;
 
     extraPackages = with pkgs; [
-       # nvchad plugins
-       ripgrep
-       nil     
-       lua-language-server
-       xclip   
+      # nvchad plugins
+      ripgrep
+      nil
+      lua-language-server
+      xclip
 
-       clang-tools
-       # rust-analyzer
-       lua-language-server
-       lombok
-       # jdt-language-server
-       nodejs
-       yarn
-       tree-sitter
+      clang-tools
+      pyright
+      # rust-analyzer
+      rustfmt
+      nixfmt
+      lua-language-server
+      lombok
+      # jdt-language-server
+      nodejs
+      yarn
+      tree-sitter
 
-       # vim plugins
-       vimPlugins.lazygit-nvim
-       vimPlugins.plenary-nvim
-       vimPlugins.vim-visual-multi
-       vimPlugins.neoformat
-       vimPlugins.markdown-preview-nvim
-       # vimPlugins.nvim-jdtls
+      # vim plugins
+      vimPlugins.lazygit-nvim
+      vimPlugins.plenary-nvim
+      vimPlugins.vim-visual-multi
+      vimPlugins.markdown-preview-nvim
+      # vimPlugins.nvim-jdtls
 
-       # golang
-       # gopls
-       # gotools 
-       # delve
-       # go
+      # golang
+      # gopls
+      # gotools
+      # delve
+      # go
 
-       # dependencies for nvchad
-       unzip
-       wl-mirror
-       rsync
-       _7zz
-       gnupg
-       pinentry-tty
+      # dependencies for nvchad
+      unzip
+      wl-mirror
+      rsync
+      _7zz
+      gnupg
+      pinentry-tty
+
+      # Vue + TypeScript LSP
+      vtsls
+      vue-language-server
+      prettierd
+      prettier
+      eslint_d
     ];
 
   };
