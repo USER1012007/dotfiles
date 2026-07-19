de07203c1e032c70ea93807760165f12b285b515
User1012007 <rojasbadilloe@gmail.com>
2026-07-19 14:13:58 -0600

    packages.nix: format and remove codex

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -7,112 +7,114 @@ let
 in
 {
   environment.systemPackages = with pkgs; [
-     git
-     niri
-     fastfetch
-     firefox
-     # thunar
-     # thunar-volman
-     pipewire
-     xwayland-satellite
-     htop
-     foot
-     clang
-     wl-clipboard
-     wlr-randr
-     gvfs
-     qalculate-gtk
-     # libreoffice
-     localsend
-     waybar
-     mpv
-     gnome-themes-extra
-     pkg-config
-     fontconfig
-     kickoff
-     swayosd
-     nomacs
-     swaylock
-     gcc
-     # linux-wifi-hotspot
-     bluez
-     grim
-     imagemagick
-     libGLU
-     curl
-     nethogs
-     yazi
-     gammastep
-     waypaper
-     awww
-     jq
-     jaq
-     ffmpeg
-     cmus
-     kdePackages.okular
-     direnv
-     ouch
-     xdg-utils
-     xdg-desktop-portal
-     xdg-desktop-portal-wlr
-     eza
-     fzf
+    git
+    niri
+    fastfetch
+    firefox
+    # thunar
+    # thunar-volman
+    pipewire
+    xwayland-satellite
+    htop
+    foot
+    clang
+    wl-clipboard
+    wlr-randr
+    gvfs
+    qalculate-gtk
+    # libreoffice
+    localsend
+    waybar
+    mpv
+    gnome-themes-extra
+    pkg-config
+    fontconfig
+    kickoff
+    swayosd
+    nomacs
+    swaylock
+    gcc
+    # linux-wifi-hotspot
+    bluez
+    grim
+    imagemagick
+    libGLU
+    curl
+    nethogs
+    yazi
+    gammastep
+    waypaper
+    awww
+    jq
+    jaq
+    ffmpeg
+    cmus
+    kdePackages.okular
+    direnv
+    ouch
+    xdg-utils
+    xdg-desktop-portal
+    xdg-desktop-portal-wlr
+    eza
+    fzf
 
-     # IDE's
-     # android-tools
-     neovim
-     # godot_4
-     # gdtoolkit_4
+    # IDE's
+    # android-tools
+    neovim
+    # godot_4
+    # gdtoolkit_4
 
-     # Language servers
-     # kotlin
-     # flutter
-     # rustc
-     # cargo
-     # ciscoPacketTracer 
-     python3
-     openjdk
+    # Language servers
+    # kotlin
+    # flutter
+    # rustc
+    # cargo
+    # ciscoPacketTracer
+    python3
+    openjdk
 
-     logseq
-     gh
+    logseq
+    gh
 
-     conda
-     gemini-cli
-     codex
+    conda
 
-     # tuis
-     wiremix
-     bluetui
-     lazygit
-     lazyssh
-     # lazyworktree
+    # agents
+    gemini-cli
+    # codex
+    opencode
 
-     # waybar
-     wl-screenrec
-     libnotify
-     gsimplecal
+    # tuis
+    wiremix
+    bluetui
+    lazygit
+    lazyssh
+    # lazyworktree
 
+    # waybar
+    wl-screenrec
+    libnotify
+    gsimplecal
 
-     # vms
-     libvirt
-     virt-manager
-     virt-viewer
+    # vms
+    libvirt
+    virt-manager
+    virt-viewer
 
-     # notifications
-     mako
+    # notifications
+    mako
 
-     # Flatpak programs scripts
-       (writeShellScriptBin "bedrock" ''
-        #!/bin/bash
-        flatpak run --env=__NV_PRIME_RENDER_OFFLOAD=1 --env=__GLX_VENDOR_LIBRARY_NAME=nvidia io.mrarm.mcpelauncher
-      '') 
+    # Flatpak programs scripts
+    (writeShellScriptBin "bedrock" ''
+      #!/bin/bash
+      flatpak run --env=__NV_PRIME_RENDER_OFFLOAD=1 --env=__GLX_VENDOR_LIBRARY_NAME=nvidia io.mrarm.mcpelauncher
+    '')
 
-       (writeShellScriptBin "jellyfin" ''
-        #!/bin/bash
-        flatpak run --env=__NV_PRIME_RENDER_OFFLOAD=1 --env=__GLX_VENDOR_LIBRARY_NAME=nvidia com.github.iwalton3.jellyfin-media-player
-      '') 
+    (writeShellScriptBin "jellyfin" ''
+      #!/bin/bash
+      flatpak run --env=__NV_PRIME_RENDER_OFFLOAD=1 --env=__GLX_VENDOR_LIBRARY_NAME=nvidia com.github.iwalton3.jellyfin-media-player
+    '')
 
-     # wf-recorder # to record screen # wf-recorder --audio=alsa_output.usb-Razer_Razer_Kraken_V3_X_00000000-00.pro-output-0.monitor --c=H.264 --file=recording.mp4
+    # wf-recorder # to record screen # wf-recorder --audio=alsa_output.usb-Razer_Razer_Kraken_V3_X_00000000-00.pro-output-0.monitor --c=H.264 --file=recording.mp4
   ];
 
 }
