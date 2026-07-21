bf71c20effa9ba1e9163fd16b24f09750c3caeb2
User1012007 <rojasbadilloe@gmail.com>
2026-07-21 16:13:54 -0600

    move

---
+++ scripts/music/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Spotify playlist audio tools";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              curl
+              ffmpeg-headless
+              jq
+              opus-tools
+              python3
+              rsgain
+              spotdl
+              yt-dlp
+            ];
+          };
+        }
+      );
+    };
+}
